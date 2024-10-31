94ced508fd70dac8d472d8c8dbdf018b46f92cbe
Gunasekaran29 <sguna5910@gmail.com>
2024-10-31 10:37:47 +0530

    inverse descending program

--- pattern.java
+++ pattern.java
@@ -1,10 +1,10 @@
 class Pattern{
 public static void main(String[] args){
 for(int i=1;i<=5;i++){
-for(int j=i;j<=5;j++){
+for(int j=1;j<=i;j++){
 System.out.print("  ");
 }
-for(int k=1;k<=i;k++){
+for(int k=i;k<=5;k++){
 System.out.print("* ");
 }
 System.out.println("");
