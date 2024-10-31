3f2dcbf33b22139778705ad7ac2f6b0af6c584ef
Gunasekaran29 <sguna5910@gmail.com>
2024-10-31 11:00:21 +0530

    inverse pyramid program

--- pattern.java
+++ pattern.java
@@ -1,13 +1,13 @@
 class Pattern{
 public static void main(String[] args){
 for(int i=1;i<=5;i++){
-for(int j=i;j<=5;j++){
+for(int j=1;j<=i;j++){
 System.out.print("  ");
 }
-for(int k=1;k<=i;k++){
+for(int k=i;k<=5;k++){
 System.out.print("* ");
 }
-for(int l=2;l<=i;l++){
+for(int l=i;l<=4;l++){
 System.out.print("* ");
 }
 System.out.println("");
