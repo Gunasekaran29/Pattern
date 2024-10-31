8aa4464045abda0df9bf90239e607ebda5c671b1
Gunasekaran29 <sguna5910@gmail.com>
2024-10-31 11:05:53 +0530

    inverse pyramid with num program

--- pattern.java
+++ pattern.java
@@ -5,10 +5,10 @@ for(int j=1;j<=i;j++){
 System.out.print("  ");
 }
 for(int k=i;k<=5;k++){
-System.out.print("* ");
+System.out.print(k+" ");
 }
-for(int l=i;l<=4;l++){
-System.out.print("* ");
+for(int l=4;l>=i;l--){
+System.out.print(l+" ");
 }
 System.out.println("");
 }
