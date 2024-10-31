a7ee6c4956ad1c2391f4c45dd6ca944e92961cb8
Gunasekaran29 <sguna5910@gmail.com>
2024-10-31 11:21:07 +0530

    hollow rectangle program

--- pattern.java
+++ pattern.java
@@ -1,14 +1,13 @@
 class Pattern{
 public static void main(String[] args){
 for(int i=1;i<=5;i++){
-for(int j=1;j<=i;j++){
+for(int j=1;j<=5;j++){
+if(i==1||i==5||j==1||j==5){
+System.out.print("* ");
+}
+else{
 System.out.print("  ");
 }
-for(int k=i;k<=5;k++){
-System.out.print(k+" ");
-}
-for(int l=4;l>=i;l--){
-System.out.print(l+" ");
 }
 System.out.println("");
 }
