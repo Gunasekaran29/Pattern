3fc176b8654f1edfbf5d944e5717d346f2e7e7b1
Gunasekaran29 <sguna5910@gmail.com>
2024-10-31 10:44:29 +0530

    pyramid

--- pattern.java
+++ pattern.java
@@ -1,10 +1,13 @@
 class Pattern{
 public static void main(String[] args){
 for(int i=1;i<=5;i++){
-for(int j=1;j<=i;j++){
+for(int j=i;j<=5;j++){
 System.out.print("  ");
 }
-for(int k=i;k<=5;k++){
+for(int k=1;k<=i;k++){
+System.out.print("* ");
+}
+for(int l=2;l<=i;l++){
 System.out.print("* ");
 }
 System.out.println("");
